0b5898bbdcbd6ad5e91e406915f8e1aec89ba73a
jasonleeprill <jasonleeprill@outlook.com>
2020-08-11 00:24:32 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+
+    var input = "jasonlee@prill.net";
+    for(var i = 0; i < input.length; i++) {
+        
+
+        if (input.charAt(i) === "@")  {
+            console.log("There's an @ sign at index " + i);
+        }
+
+
+
+
+
+    }
+        
