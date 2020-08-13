1863c2b604b7fa23280b492d0799fd688530a44f
Jason Lee Prill <jasonleeprill@outlook.com>
2020-08-12 23:23:27 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,43 @@
 
-    var input = "jasonlee@prill.net";
-    for(var i = 0; i < input.length; i++) {
-        
+var http = require('http'),
+    fs = require('http');
 
-        if (input.charAt(i) === "@")  {
-            console.log("There's an @ sign at index " + i);
+function serveStaticFile(res, path, contentType, responseCode) {
+    if(!responseCode) responseCode = 200;
+    fs.readFile(__dirname + path, function(err,data) {
+        if (err) { 
+            res.writeHead(500, { 'Content-Type': 'text/plain' });
+            res.end('500 - Internal Error');
+        } else {
+                res.writeHead(responseCode,
+                    { 'Content-Type': contentType });
+                    res.end(data);
+                }
+            });
         }
 
-
-
-
-
+http.createServer(function(req,res){
+    // mormalize url by removing querystring, optional
+    // trailing slash, and making lowercase
+    var path = req.url.replace(/\/?(?:\?.)?$/, "")
+        .toLowerCase();
+    switch(path) {
+        case '':
+            serverStaticFiles(res, '/public/home.html', 'text/html');
+            break;
+        case '/about':
+            serveStaticFiles(res, '/publice/about.html', 'text/html');
+            break;
+        case '/img/logo.jpg':
+            serveStaticFile(res, '/public/img/logo.jpg',
+                    'image/jpeg');
+            break;
+        default:
+            serveStaticFile(res, '/public/404.html', 'text/html',
+                    404);
+            break;
     }
+}).listen(3000);
+console.log('Server started on localhost:3000; press Ctrl-C to terminate....');
+
         
